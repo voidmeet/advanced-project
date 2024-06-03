220dff4ae7f569bab7d418ef841816ede21b9f65
Meet Bundela <meetlearns@gmail.com>
2024-06-03 17:17:36 +0530

    one more

--- .github/workflows/setup.py
+++ .github/workflows/setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='advanceproject',
-    version='V1.0',
-    packages=[],  # Add package names if applicable
+    name='your-project-name',
+    version='0.1',
+    packages=['advanced-project'],  # Replace 'your_package_name' with the name of your Python package
+    author='meet bundela',
+    author_email='meetlearns@gmail.com',
+    description='A short description of your project',
+    url='https://github.com/voidmeet/advanced-project.git',
+    # Add other metadata as needed
 )
-advanced-project
